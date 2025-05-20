8cf62d18baf85b5e2cf8d96e9ac0f51f74a117e5
wilia <wilia.francisco@souunit.com.br>
2025-05-20 20:48:49 -0300

    d

--- dashboard.py
+++ dashboard.py
@@ -268,7 +268,7 @@ def paginaatos():
 
                 fig = go.Figure()
 
-                df_vendas["Valor_formatado"] = df_vendas["Vendas"].apply(lambda y: lc.currency(y, grouping=True))
+                df_vendas["Valor_formatado"] = df_vendas["Vendas"].apply(lambda y: formatar_moeda(y))
 
                 fig.add_trace(go.Scatter(
                     x=df_vendas["Mês"].dt.strftime('%m/%Y'),
